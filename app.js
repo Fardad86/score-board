e7b8fb214626a6bdddcc882288625c5948b18501
Fardad86 <136969613+Fardad86@users.noreply.github.com>
2024-08-24 23:18:40 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
-// Import Firebase functions
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { createClient } from '@supabase/supabase-js';
 
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDkfWwCITUJf1qR-3A2KD-xwf87ho98GTc",
-    authDomain: "lig-score-board.firebaseapp.com",
-    projectId: "lig-score-board",
-    storageBucket: "lig-score-board.appspot.com",
-    messagingSenderId: "993583824589",
-    appId: "1:993583824589:web:0f9bb30c2bf766b238f384"
-};
+const supabaseUrl = 'https://pyecsyykgzeionihrhwi.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZWNzeXlrZ3plaW9uaWhyaHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1MjIzNzMsImV4cCI6MjA0MDA5ODM3M30.oBNxX9Hl-89r_aCrzLJwbkdtdJB-e7rhOmhZd0q9RUc';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Handle login
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
 
     loginForm.addEventListener('submit', async function(event) {
         event.preventDefault();
-        const email = document.getElementById('email').value;
+        const email = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
         try {
-            await signInWithEmailAndPassword(auth, email, password);
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password
+            });
+
+            if (error) throw error;
+
             window.location.href = "admin.html"; // Redirect to admin panel
         } catch (error) {
             console.error("Error logging in: ", error);
-            alert("Failed to log in. Please check your credentials.");
+            document.getElementById('error-message').textContent = "Failed to log in. Please check your credentials.";
         }
     });
 });
