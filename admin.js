44bc90e878d95197b48af9db00c7fb07d9e3008d
Fardad86 <136969613+Fardad86@users.noreply.github.com>
2024-08-25 01:28:50 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
 // اتصال به Supabase
 const supabaseUrl = 'https://pyecsyykgzeionihrhwi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZWNzeXlrZ3plaW9uaWhyaHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1MjIzNzMsImV4cCI6MjA0MDA5ODM3M30.oBNxX9Hl-89r_aCrzLJwbkdtdJB-e7rhOmhZd0q9RUc';
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey); // استفاده از window برای اطمینان از مقداردهی
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // ساخت تیم جدید
 document.getElementById('create-team-form').addEventListener('submit', async (e) => {
