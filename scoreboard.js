0955b13e435401b0c99a6ffecf0b14db6af448f3
Fardad86 <136969613+Fardad86@users.noreply.github.com>
2024-08-25 11:47:24 +0330

    Update scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initialize Supabase client
     const supabaseUrl = 'https://pyecsyykgzeionihrhwi.supabase.co';
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZWNzeXlrZ3plaW9uaWhyaHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1MjIzNzMsImV4cCI6MjA0MDA5ODM3M30.oBNxX9Hl-89r_aCrzLJwbkdtdJB-e7rhOmhZd0q9RUc';
-    const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+    const supabase = window.supabase.createClient(supabaseUrl, supabaseKey); // تغییر این خط
 
     const refreshButton = document.getElementById('refresh-btn');
     const scoreBoard = document.getElementById('score-board');
