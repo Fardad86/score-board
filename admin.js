a5e166fce7d9ec3e1af558d45a02e47d1d5bb31e
Fardad86 <136969613+Fardad86@users.noreply.github.com>
2024-08-25 12:57:43 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,10 @@
-// Initialize Supabase client
+// admin.js
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
 const supabaseUrl = 'https://pyecsyykgzeionihrhwi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZWNzeXlrZ3plaW9uaWhyaHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1MjIzNzMsImV4cCI6MjA0MDA5ODM3M30.oBNxX9Hl-89r_aCrzLJwbkdtdJB-e7rhOmhZd0q9RUc';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 document.addEventListener('DOMContentLoaded', () => {
     const createTeamForm = document.getElementById('create-team-form');
